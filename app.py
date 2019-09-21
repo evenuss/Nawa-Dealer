e7f456b396b050dd241233bbc3d4b0da74d7e021
evenuss <ucoksilalahi345@gmail.com>
2019-09-21 21:21:54 +0700

    Revisi

--- app.py
+++ app.py
@@ -102,12 +102,12 @@ def add_customer():
 
 
 
-@app.route('/protected', methods=['GET'])
-@jwt_required
-def protected():
-    # Access the identity of the current user with get_jwt_identity
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
+# @app.route('/protected', methods=['GET'])
+# @jwt_required
+# def protected():
+#     # Access the identity of the current user with get_jwt_identity
+#     current_user = get_jwt_identity()
+#     return jsonify(logged_in_as=current_user), 200
 #################################################################################3
 
 
